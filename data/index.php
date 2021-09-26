2aee06e0097cc29566b4c28871639121c02fe080
luffyzhao <luffyzhao@vip.126.com>
2021-09-26 16:06:13 +0800

    示例

--- data/index.php
+++ data/index.php
@@ -1,4 +1,29 @@
 <?php
+
+use LCustoms\Entry\CEB311Message;
+use LCustoms\Entry\Transfer;
+use LCustoms\Message;
+
 include_once __DIR__ . '/../vendor/autoload.php';
 
 
+$orderHead = new CEB311Message\OrderHead();
+$orderHead->ebcName = "aaa";
+//  参数
+
+$ceb311 = new CEB311Message($orderHead);
+
+$list = new CEB311Message\OrderList();
+// 参数
+
+
+$ceb311->addOrderList(
+    $list
+);
+
+
+$message = new Message(
+    new Transfer
+);
+
+echo (string)$message->map([$ceb311], 'CEB311Message');
